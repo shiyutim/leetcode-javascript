2d56254e899ad129b32c9bd7255e1f00f57acf32
shiyutim <shiyutim@outlook.com>
2020-08-16 22:53:53 +0800

    add 485

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'leetcode for javascript',
+    description: '使用 JavaScript 解 leetcoede',
+    themeConfig: {
+        sidebar: [
+            {
+              title:  '说明',   // 必要的
+            //   path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 1,    // 可选的, 默认值是 1
+              children: [
+                '/'
+              ]
+            },
+            {
+              title: '401-500',   // 必要的
+            //   path: '/401-500/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+              collapsable: false, // 可选的, 默认值是 true,
+              sidebarDepth: 1,    // 可选的, 默认值是 1
+              children: [
+                  '401-500/401',
+                '401-500/485',
+              ]
+            },
+          ]
+      }
+  }
